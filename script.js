2037be3678497db3ae5d5ab0671e7604cb088c2f
codydecoder <codycad1@yahoo.com>
2024-01-17 16:00:34 -0600

    Grade Assigner

--- script.js
+++ script.js
@@ -8,10 +8,34 @@ else {
 }
 */
 //Team Player
-let softballTeam = ["Who", "What", "I Don't Know", "Why", "Because", "Tomorrow", "Today", "Nobody", "I Don't Care"];
+/*let softballTeam = ["Who", "What", "I Don't Know", "Why", "Because", "Tomorrow", "Today", "Nobody", "I Don't Care"];
 if(softballTeam.length >= 9) {
     console.log(`Play ball!     with ${softballTeam.length} players`)
 }
 else {
     console.log(`not enough players     ${(9 - softballTeam.length)} players short`)
+}
+*/
+//Grade Assigner
+let grade = 100;
+if(grade > 100 || grade < 0) {
+    console.log(`Please enter a valid grade(0-100)`)
+}
+else if(grade >= 90) {
+    console.log(`A`)
+}
+else if(grade >= 80) {
+    console.log(`B`)
+}
+else if(grade >= 70) {
+    console.log(`C`)
+}
+else if(grade >= 65) {
+    console.log(`D`)
+}
+else if(grade >= 0) {
+    console.log(`F`)
+}
+else {
+    console.log(`Please enter a valid grade(number value 0-100)`)
 }
