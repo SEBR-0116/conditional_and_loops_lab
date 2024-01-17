00504ccf10533956ebb163f4f15fe73ba05601a9
persefy <stephaniejcancel@gmail.com>
2024-01-17 16:46:01 -0500

    Prompt 1 - Weather Report

---
+++ script.js
@@ -0,0 +1,9 @@
+//Prompt 1 - Weather Report
+//Create a variable called Temperature and set a number to its value, then create a conditional that measures this temperature. If the temperature is above 75, log "it is warm" to your console. If not, log "it is cold"
+let temperature = 35;
+
+if (temperature > 75) {
+    console.log("It is warm");
+} else {
+    console.log("It is cold");
+}
