0425bfdecf64940a3f47330a739cf90cb779e010
codydecoder <codycad1@yahoo.com>
2024-01-17 15:51:12 -0600

    Team Player

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 //Weather Report
-let Tempurature = 0;
+/*let Tempurature = 0;
 if(Tempurature >= 75) {
     console.log(`it is warm, ${Tempurature} degrees to be precise`)
 }
 else {
     console.log(`it is cold, ${Tempurature} degrees to be precise`)
+}
+*/
+//Team Player
+let softballTeam = ["Who", "What", "I Don't Know", "Why", "Because", "Tomorrow", "Today", "Nobody", "I Don't Care"];
+if(softballTeam.length >= 9) {
+    console.log(`Play ball!     with ${softballTeam.length} players`)
+}
+else {
+    console.log(`not enough players     ${(9 - softballTeam.length)} players short`)
 }
