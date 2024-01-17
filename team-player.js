fce4334f288062a8d40dcbe9164281cad059cebc
Zekkune <christian.adams22405@gmail.com>
2024-01-17 18:47:54 -0500

    2 - team-player

---
+++ team-player.js
@@ -0,0 +1,7 @@
+const myTeam = ['Jordan', 'Aubrey', 'Terry', 'Dick', 'Bucky', 'Chris', 'Corey', 'Trey', 'Marley']
+
+if (myTeam.length > 9) {
+    console.log("Play ball!")
+} else {
+    console.log("not enough players")
+}
