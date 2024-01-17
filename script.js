f7c9c1d5a482a94f8c31246f99b66b193b40122a
itzel <itzelalwarafii@gmail.com>
2024-01-17 14:57:33 -0800

    Team Player

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-let temperature = 65
+// let temperature = 65
 
-temperature > 75 ? console.log('it is warm') : console.log("is is cold")
+// temperature > 75 ? console.log('it is warm') : console.log("is is cold")
 
+                          //**********
+                          //Team Player 
+
+let softballTeam = ['Mario','Andrew','Moe','Sergio','Miguel','David','Nick','Mark','Diego']
+
+//using Ternary Operator
+softballTeam.length >= 9 ? console.log('Play ball!') :  console.log('not enough players')
+
+/* using if/else
+
+if (softballTeam.length >= 9 ){
+ console.log('Play ball!')
+
+} else{
+console.log('not enough players')
+} 
+
+*/ 
+
+
+
+        
+ 
 
