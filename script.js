17f878ee704ca41024d220d87ff889f608649f37
itzel <itzelalwarafii@gmail.com>
2024-01-17 15:38:22 -0800

    Grade Assinger

--- script.js
+++ script.js
@@ -5,10 +5,11 @@
                           //**********
                           //Team Player 
 
-let softballTeam = ['Mario','Andrew','Moe','Sergio','Miguel','David','Nick','Mark','Diego']
+//let softballTeam = ['Mario','Andrew','Moe','Sergio','Miguel','David','Nick','Mark','Diego']
 
-//using Ternary Operator
+/*using Ternary Operator
 softballTeam.length >= 9 ? console.log('Play ball!') :  console.log('not enough players')
+*/
 
 /* using if/else
 
@@ -20,9 +21,35 @@ console.log('not enough players')
 } 
 
 */ 
+                                 //**********
+                             /* 3-GRADE ASSIGNER */
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('Enter your grade  ', grade => {
+    if ( grade  >= 90 ){
+       console.log('A') 
+    } else if ( grade >=80 ){
+        console.log('B')
+    }
+    else if ( grade >=79){
+        console.log('c')
+    }
+   else if (grade >= 69 ){
+    console.log('D')
+   }
+   else if (grade < 65){
+    console.log('F')
+    
+   }
+   else {
+    console.log('Please enter a valid grade')
+   }
+   readline.close();
+})
 
 
-
-        
- 
 
