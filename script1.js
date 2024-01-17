42e80fe3b06471884ded8ec994c7f5ba69e39e1d
AbeGue02 <abrahamgue02@gmail.com>
2024-01-17 16:38:06 -0500

    Adds scripts 1 and 2

---
+++ script1.js
@@ -0,0 +1,3 @@
+let temperature = 79
+
+temperature > 75 ? console.log('It is warm') : console.log('It is cold')
---
+++ script2.js
@@ -0,0 +1,3 @@
+const teamMembers = ['Abraham Guerrero', 'Joy Wu', 'Jeremy Taubman']
+
+teamMembers.length >= 9 ? console.log('Play Ball!') : console.log('Not enough players')
