dabec8764d51dc2625137c70cbfdca9d99dbd749
whoever11 <cjthesedays000@gmail.com>
2024-01-17 17:52:29 -0500

    first project done

---
+++ script.js
@@ -0,0 +1,21 @@
+//1 - Weather Report
+
+ let Temperature = 88;
+
+ if (Temperature > 75){
+     console.log("it is warm");
+ }
+ else { 
+     console.log("it is cold");
+ }
+//end of Weather Report 
+
+//2 - Team Player
+
+// let softballTeam = ["Alex", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Henry", "Mark"];
+
+// if (softballTeam.length >= 9) {
+//     console.log("Play ball!");
+// } else {
+//     console.log("Not enough players");
+// }
