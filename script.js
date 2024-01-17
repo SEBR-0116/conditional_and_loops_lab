ca0ce19b580559e855475d73134c4150562d5da6
codydecoder <codycad1@yahoo.com>
2024-01-17 15:41:20 -0600

    Weather Report

---
+++ script.js
@@ -0,0 +1,8 @@
+//Weather Report
+let Tempurature = 0;
+if(Tempurature >= 75) {
+    console.log(`it is warm, ${Tempurature} degrees to be precise`)
+}
+else {
+    console.log(`it is cold, ${Tempurature} degrees to be precise`)
+}
