7f41b2feeb2b489db4a35900c633b286afa5ed24
Zekkune <christian.adams22405@gmail.com>
2024-01-17 19:59:18 -0500

    7 - foods

---
+++ foods.js
@@ -0,0 +1,7 @@
+let foods = ['fudge', 'cheese', 'strawberrie', 'pinneapple', 'burrito', 'pizza', 'funnel cake', 'falafel', 'french omlet']
+
+for (let i = 0; i < foods.length; i++) {
+    if (foods[i].charAt(0) === 'f') {
+        console.log(foods[i])
+    }
+}
