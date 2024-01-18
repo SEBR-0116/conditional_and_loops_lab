de3b5d473169e891823cff4a0a68150786da3cbf
shubham20242 <stekawad@mtu.edu>
2024-01-18 06:54:57 -0500

    Team_Player

---
+++ Team_Player.js
@@ -0,0 +1,10 @@
+let names = ["Shubham","Abhi","Akshay","Sneha","Prajkta","Poonam","Amit","Shivansh","Parth"]
+
+if (names.length>=9)
+{
+    console.log("Play ball!")
+}
+else
+{
+    console.log("not enough players")
+}
