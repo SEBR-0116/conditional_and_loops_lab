db0cd7c1bc111d989a567a6379b0ed13ef55aaaa
shubham20242 <stekawad@mtu.edu>
2024-01-18 07:54:29 -0500

    FFF_Foods

---
+++ FFF_Foods.js
@@ -0,0 +1,10 @@
+foods =["Biryani","FrenchFries","Milk","Pizza","FriedChicken","Falafel"]
+
+for (let i=0;i<foods.length;i++)
+{
+    if(foods[i].charAt(0)==="F")
+    {
+        console.log(foods[i])
+    }
+   
+}
