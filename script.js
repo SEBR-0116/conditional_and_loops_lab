7432adb8aa5624a13c1284cf20525542798a2b8c
Felix <felixwhman@gmail.com>
2024-01-18 01:23:52 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,86 @@
+//1-Weather Report
+
+// let temperature = 26
+
+// if (temperature > 95) {
+//     console.log('it is warm')
+// } else {
+//     console.log('it is cold')
+// }
+
+//2-Team Player
+
+// let softballTeam = ['Mike', 'Kevin', 'Wallce', 'Brandyn', 'Nick', 'Jason']
+
+// if (softballTeam.length >= 9) {
+//     console.log('Play ball!')
+// } else {
+//     console.log('not enough players')
+// }
+
+//3-Grade Assigner
+
+// let grade = undefined
+
+// if (isNaN(grade)) {
+//     console.log('Please enter a valid grade')
+// } else if (grade >= 90) {
+//     console.log('A')
+// } else if (grade >= 80) {
+//     console.log('B')
+// } else if (grade >= 70) {
+//     console.log('C')
+// } else if (grade >= 65) {
+//     console.log('D')
+// } else if (grade <= 65) {
+//     console.log('Failing')
+// } 
+
+//4-ATM Function
+
+// let checkingAccount = 1000
+// let savingsAccount = 10000
+// let action = 'Withdraw'
+
+// switch (action) {
+//     case 'Withdraw':
+//         checkingAccount -= 100;
+//         console.log('Withdrew $100 from checking account')
+//         break
+//     case 'Deposit':
+//         checkingAccount += 100
+//         console.log('Deposited $100 into checking account')
+//         break
+//     case 'Transfer':
+//         checkingAccount -= 100
+//         savingsAccount += 100
+//         console.log('Transferred $100 from checking into savings account')
+//         break
+//     default:
+//         console.log('Invalid action type')
+// }
+
+//5 - High Five
+
+// for (let i = 0; i < 50; i+5) {
+//     i += 5
+//     console.log(i)
+// }
+
+//6 - Its Hip to Loop Squares
+// let result = 0
+// for (let i = 0; i <= 10; i++) {
+//     result = i * i
+//     console.log(result)
+// }
+
+//7 - F F F Foods
+// let foods = ['pizza', 'falafel', 'sushi', 'french fries']
+// let favFoods = []
+
+// for (let i = 0; i < foods.length; i++) {
+//     if (foods[i].charAt(0) == 'f') {
+//         favFoods.push(foods[i])
+//     }
+// }
+// console.log(favFoods)
