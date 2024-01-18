ec03ae7089292a42df88390fe20e9456025d14c3
VenomSymbiotes <156369944+VenomSymbiotes@users.noreply.github.com>
2024-01-18 09:27:15 -0500

    hw submission

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ let temperature = 45;
 
 switch (true) {
     case temperature <= 32:
-        console.log("It's Brick Outside Today");
+       console.log("It's Brick Outside Today");
         break;
         default:
             console.log("It's Ight")
+           break;
+}
+
+let teammates = ['Tony','Angel','Miguel','Justin','Ernst','Ryan','Jorre','Antoon','Etan','Grant','David']
+console.log(teammates.length);
+
+switch (true) {
+    case teammates.length >= 9:
+        console.log("Play Ball!");
+        break;
+        default:
+            console.log("Not Enough Players")
             break;
 }
