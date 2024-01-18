53e8a0e785d59d057957e529af4c5bd3136345fe
larsonscott89 <scottlarson0305@gmail.com>
2024-01-18 09:26:53 -0500

    hw submission

---
+++ conditional_loops.js
@@ -0,0 +1,92 @@
+// Prompt 1: Weather Report
+
+// let temperature = 74
+
+// if (temperature > 75) {
+//   console.log(`It is warm`)
+//  } else {
+//   console.log(`It is cold`)
+// }
+
+/////////////////////////////////
+// Prompt 2: Team Player
+
+// let team = ['Calvin', 'Scott', 'Jeff', 'Bob', 'Mal', 'Chris', 'Jimmy', 'Billy', 'Phil']
+// let arrayLength = team.length
+// if (arrayLength < 9) {
+//   console.log('Not enough players!')
+// } else {
+//   console.log('Play ball!')
+// }
+
+////////////////////////////////
+// Prompt 3: Grade Assigner
+
+// let grade = 101
+
+// if (grade <=100 && grade >=90) {
+//   console.log('You have an A!')
+// } else if (grade < 90 && grade >= 80) {
+//   console.log('You have a B!')
+// } else if (grade < 80 && grade >= 70) {
+//   console.log('You have a C.')
+// } else if (grade < 70 && grade >= 65) {
+//   console.log('You have a D')
+// } else if (grade <= 64 && grade >= 0) { 
+//   console.log('Failing Mark')
+// } else 
+// console.log('Please enter a valid grade')
+
+////////////////////////////////////////////////
+// Prompt 4: ATM Function
+
+// let checkingAccount = 2000
+// let savingsAccount = 500
+// let action = ['deposit', 'withdraw', 'transfer']
+
+// switch (action[2]) {
+//   case 'withdraw':
+//     checkingAccount -= 100;
+//     console.log('Withdrawn $100 from Checking Account.')
+//     break;
+//   case 'deposit':
+//     savingsAccount += 100;
+//     console.log('Deposited $100 into Savings Account')
+//     break;
+//   case 'transfer':
+//     checkingAccount -= 100;
+//     savingsAccount += 100;
+//     console.log('You have transferred $100 from your Checking Account into your Savings Account')
+//     break;
+//   default:
+//     console.log('Please choose a correct banking action')
+// }
+
+// console.log("Checking Account Balance:", checkingAccount);
+// console.log("Savings Account Balance:", savingsAccount);
+
+///////////////////////////////////////////////
+// Prompt 5: High Five!
+
+// for (let i = 0; i <= 50; i += 5) {
+//   console.log(i)
+// }
+
+////////////////////////////////////////////////
+// Prompt 6: Its Hip to Loop Squares
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i * i)
+// } 
+
+////////////////////////////////////////////////
+// Prompt 7: F F F Foods
+
+// const foods = ['Pizza', 'Fettuccine', 'Soup', 'Pasta', 'Chicken', 'Fondue', 'Falafel', 'Hamburger', 'Mashed Potatos', 'Cereal', 'French Fries', 'Salmon', 'Grapes', 'Cheese', 'Fish', 'Ramen', 'Salad',]
+
+// for (let i = 0; i < foods.length; i++) {
+//   if (foods[i].charAt(0) == 'f' || foods[i].charAt(0) === 'F') {
+//     console.log(foods[i])
+//     }
+//   }
+
