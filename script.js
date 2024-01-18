573a46c750a5eed17ff95b35d9956ebbcbf48f43
itzel <itzelalwarafii@gmail.com>
2024-01-17 17:20:12 -0800

    ATM Function

--- script.js
+++ script.js
@@ -24,32 +24,58 @@ console.log('not enough players')
                                  //**********
                              /* 3-GRADE ASSIGNER */
 
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// readline.question('Enter your grade  ', grade => {
+//     if ( grade  >= 90 ){
+//        console.log('A') 
+//     } else if ( grade >=80 ){
+//         console.log('B')
+//     }
+//     else if ( grade >=79){
+//         console.log('c')
+//     }
+//    else if (grade >= 69 ){
+//     console.log('D')
+//    }
+//    else if (grade < 65){
+//     console.log('F')
+    
+//    }
+//    else {
+//     console.log('Please enter a valid grade')
+//    }
+//    readline.close();
+// })
+
+                                    //**********
+                             /* 4- ATM Function */
+
+  let checkAcc = 15000
+  let savAcc = 25000
+  
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-readline.question('Enter your grade  ', grade => {
-    if ( grade  >= 90 ){
-       console.log('A') 
-    } else if ( grade >=80 ){
-        console.log('B')
-    }
-    else if ( grade >=79){
-        console.log('c')
-    }
-   else if (grade >= 69 ){
-    console.log('D')
-   }
-   else if (grade < 65){
-    console.log('F')
-    
-   }
-   else {
-    console.log('Please enter a valid grade')
-   }
-   readline.close();
-})
+readline.question('Type what would you like to do:  Deposit, Withdraw, Transfer ', ActionType =>{
+    switch(ActionType){
+        case 'Withdraw':
+             console.log('Your New Checking Account Balance is  ' , checkAcc -= 100)
+        break;
+        case 'Deposit':
+            console.log('Your New Savings Account Balance is  ' ,savAcc += 100)
+            break;
+        case 'Transfer' :
+            console.log('Your new balaces are : Checking ', checkAcc -=100 )
+            console.log('Your new balaces are : Savings ', checkAcc +=100 )
+            break;
 
-
-
+    } 
+    readline.close();
+});
+  
