6290479549b4da1814047a6435330bec71b39f13
VenomSymbiotes <156369944+VenomSymbiotes@users.noreply.github.com>
2024-01-17 20:55:14 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let temperature = 45;
+
+switch (true) {
+    case temperature <= 32:
+        console.log("It's Brick Outside Today");
+        break;
+        default:
+            console.log("It's Ight")
+            break;
+}
