99f08aea7963b0cb449fb6789500fe23167741d3
cjharrell88 <cjharrell88@icloud.com>
2024-01-18 07:56:27 -0700

    hw submission

---
+++ conditional_loops.js
@@ -0,0 +1,40 @@
+// // Weather Report
+
+// let temperature = 75
+
+// if (temperature < 75) {
+//     console.log ('it is cold')
+// } else {
+//     console.log ('it is warm')
+// }
+
+//Team Player
+
+// let softballTeam = ['Jenn', 'Sally', 'Susie', 'Marie','Lisa','Ben','Jessie','Mike','Todd'];
+// let numberOfPlayers = softballTeam.length
+// if (softballTeam < 9){
+//     console.log ('not enough players')
+// } else {
+//     console.log('Play ball!')
+// }
+
+//Grade Assigner
+
+let grade = 88
+if (grade <=100 && grade >=90) {
+  console.log('You have an A!')
+} else if (grade < 90 && grade >= 80) {
+  console.log('You have a B!')
+} else if (grade < 80 && grade >= 70) {
+  console.log('You have a C.')
+} else if (grade < 70 && grade >= 65) {
+  console.log('You have a D')
+} else if (grade <= 64 && grade >= 0) {
+  console.log('Failing Mark')
+} else if (isNaN(NaN))
+console.log('Please enter a valid grade')
+
+//ATM Function
+
+// let checkingAccount = ['$10']
+// let savingsAccount = ['$20']
