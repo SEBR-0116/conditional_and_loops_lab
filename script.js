c9e019d1b19fe727a72c355c6aa4de969574f195
Stephanie Cancel <stephaniejcancel@gmail.com>
2024-01-18 09:32:43 -0500

    hw submission update

--- script.js
+++ script.js
@@ -82,7 +82,8 @@ for (let i = 0; i<=10; i++) {
 
 let food = ['fish','lettuce', 'farro'];
 for (let i = 0; i<food.length; i++) {
-  if () {
-    console.log();
+  if (food[i].charAt[0] = 'f') {
+    console.log(i);
   }
 }
+//not yet working
